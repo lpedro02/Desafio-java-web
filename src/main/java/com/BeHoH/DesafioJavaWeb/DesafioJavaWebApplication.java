79d737b3744f496aa34c145c5b9429d698add0a1
Pedro Gomes <pthiagoconstantino@gmail.com>
2020-09-27 18:20:22 -0300

    Initial commit

---
+++ src/main/java/com/BeHoH/DesafioJavaWeb/DesafioJavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.BeHoH.DesafioJavaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BeHoH/DesafioJavaWeb/DesafioJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BeHoH.DesafioJavaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
